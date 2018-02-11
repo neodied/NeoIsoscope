8ccb73031d9743e8ad682cab4f6a24b235e59081
neodied <joshua.d.wilke@gmail.com>
2018-02-10 22:13:29 -0500

    Create local cache to allow non-API access

--- shtuff.py
+++ shtuff.py
@@ -8,5 +8,24 @@ class POI:
         self.longlat_lst = [self.longitude, self.latitude]
         self.alias = alias
 
-import folium
 
+class IsolineKey:
+    def __init__(self, center_lat, center_lon, radius_sec):
+        self.center_lat = center_lat
+        self.center_lon = center_lon
+        self.radius_sec = radius_sec
+
+    def __hash__(self):
+        return hash((self.center_lat, self.center_lon, self.radius_sec))
+
+    def __eq__(self, other):
+        return (self.center_lat, self.center_lon, self.radius_sec) == (other.center_lat, other.center_lon, other.radius_sec)
+
+    def __ne__(self, other):
+        return not(self == other)
+
+
+def serialize(obj):
+    if isinstance(obj, IsolineKey):
+        serial = f"{obj.center_lat} {obj.center_lon} {obj.radius_sec}"
+        return serial
